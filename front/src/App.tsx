ce008ab24974a4570019f752358b02a4b85aaad9
Caio Souza <souzsdev@gmail.com>
2024-02-14 19:24:57 -0300

    Technology change - front

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-red-500">Ola mundo!</h1>
+        <Button>Botão</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
